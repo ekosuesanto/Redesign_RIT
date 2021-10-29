994b15ae80f0c4c9ab72eadd467be64c5129c283
FlutLab.io <>
2021-10-29 16:15:27 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+// choose fileimport 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:helloworld/onboarding.dart';
 import 'package:helloworld/main_page.dart';
