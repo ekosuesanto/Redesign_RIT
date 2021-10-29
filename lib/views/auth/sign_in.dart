ad52331cd9a9df9734c4bcbd58d21cefbd491390
FlutLab.io <>
2021-10-29 17:09:15 +0000

    Autosave

--- lib/views/auth/sign_in.dart
+++ lib/views/auth/sign_in.dart
@@ -60,6 +60,7 @@ class _SignInFormState extends State<SignInForm> {
                         ),
                         alignment: Alignment.centerLeft,
                       ),
+                      Container(child: Text("Already account? Please Sign In",style:TextStyle(fontSize:12)), 
                       SizedBox(
                         height: 40,
                       ),
