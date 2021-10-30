fc53d6e0708f2eefff34c617d61a595327f66d46
FlutLab.io <>
2021-10-30 18:46:55 +0000

    Autosave

--- lib/views/register/register_view.dart
+++ lib/views/register/register_view.dart
@@ -11,7 +11,7 @@ class RegisterView extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
         colorScheme: ColorScheme.fromSwatch(
-      primarySwatch: Colors.blue,
+      primarySwatch: Colors.whiteSmoke,
     ).copyWith(
       secondary: Colors.green,
     ),
