b5ebaca8510b16023eeae4fc9038e8939506cd68
FlutLab.io <>
2021-10-30 20:10:54 +0000

    Autosave

--- lib/views/register/register_view.dart
+++ lib/views/register/register_view.dart
@@ -54,7 +54,78 @@ class _SignUpFormState extends State<SignUpForm> {
 
   @override
   Widget build(BuildContext context) {
-      
+      final steps = [
+      CoolStep(
+        title: 'Basic Information',
+        subtitle: 'Please fill some of the basic information to get started',
+        content: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              _buildTextField(
+                labelText: 'Name',
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Name is required';
+                  }
+                  return null;
+                },
+                controller: _nameCtrl,
+              ),
+              _buildTextField(
+                labelText: 'Email Address',
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Email address is required';
+                  }
+                  return null;
+                },
+                controller: _emailCtrl,
+              ),
+            ],
+          ),
+        ),
+        validation: () {
+          if (!_formKey.currentState!.validate()) {
+            return 'Fill form correctly';
+          }
+          return null;
+        },
+      ),
+      CoolStep(
+        title: 'Select your role',
+        subtitle: 'Choose a role that better defines you',
+        content: Container(
+          child: Row(
+            children: <Widget>[
+              _buildSelector(
+                context: context,
+                name: 'Writer',
+              ),
+              SizedBox(width: 5.0),
+              _buildSelector(
+                context: context,
+                name: 'Editor',
+              ),
+            ],
+          ),
+        ),
+        validation: () {
+          return null;
+        },
+      ),
+    ];
+
+    final stepper = CoolStepper(
+      showErrorSnackbar: false,
+      onCompleted: () {
+        print('Steps completed!');
+      },
+      steps: steps,
+      config: CoolStepperConfig(
+        backText: 'PREV',
+      ),
+    );
     
       return LayoutBuilder(builder: (context, constraint) {
       return SingleChildScrollView(
@@ -91,6 +162,58 @@ class _SignUpFormState extends State<SignUpForm> {
    
           
     }
-    
+    Widget _buildTextField({
+    String? labelText,
+    FormFieldValidator<String>? validator,
+    TextEditingController? controller,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20.0),
+      child: TextFormField(
+        decoration: InputDecoration(
+          labelText: labelText,
+        ),
+        validator: validator,
+        controller: controller,
+      ),
+    );
+  }
+
+  Widget _buildSelector({
+    BuildContext? context,
+    required String name,
+  }) {
+    final isActive = name == selectedRole;
+
+    return Expanded(
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 200),
+        curve: Curves.easeInOut,
+        decoration: BoxDecoration(
+          color: isActive ? Theme.of(context!).primaryColor : null,
+          border: Border.all(
+            width: 0,
+          ),
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: RadioListTile(
+          value: name,
+          activeColor: Colors.white,
+          groupValue: selectedRole,
+          onChanged: (String? v) {
+            setState(() {
+              selectedRole = v;
+            });
+          },
+          title: Text(
+            name,
+            style: TextStyle(
+              color: isActive ? Colors.white : null,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
     
   }
