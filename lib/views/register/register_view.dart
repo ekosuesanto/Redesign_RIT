447dc9c3d54b34c3b1b1ac8aad2b0a50827fcf9d
FlutLab.io <>
2021-10-30 14:28:57 +0000

    Autosave

---
+++ lib/views/register/register_view.dart
@@ -0,0 +1,25 @@
+import  'package:flutter/material.dart';
+
+class RegisterView extends StatelessWidget {
+    Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: SignUpForm()),
+    );
+  }
+}
+
+class SignUpForm extends StatefulWidget {
+  const SignUpForm({Key? key}) : super(key: key);
+
+  @override
+  _SignUpFormState createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
+  
+
+  @override
+  Widget build(BuildContext context) {
+  }
+}
