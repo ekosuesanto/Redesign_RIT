c2ae0d8b9b765efa0822b4a2821c46d00910cf0e
FlutLab.io <>
2021-10-31 05:44:13 +0000

    Autosave

--- lib/views/register/register_view.dart
+++ lib/views/register/register_view.dart
@@ -88,7 +88,7 @@ class _SignUpFormState extends State<SignUpForm> {
                       Center(
                         child : SizedBox( 
                           width: 500,
-          height: MediaQuery.of(context).size.height - 350, 
+          height: MediaQuery.of(context).size.height - 340, 
                           child : MultiWizard(
                              steps: [ 
                                  WizardStep(
